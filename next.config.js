e8274039a11721358f8ef2f643a489d55be59c53
Zuum Developer <developer@zuum.app>
2025-07-13 18:24:35 +0900

    fix: remove srcDir/swcMinify, set eslint.ignoreDuringBuilds for Next.js 15

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  srcDir: 'src',
-  reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-}
-
-module.exports = nextConfig
